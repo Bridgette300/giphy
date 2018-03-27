10e510dae5dd258e8ca22ff04efe4bd4cc4962e3
Bridgette Smith <bsmith@ift.org>
2018-03-27 12:35:14 -0500

    5th commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,23 @@ let anime = ["Cowboy Bebop", "Dragonball Z", "Sailormoon", "Naruto", "One Piece"
 //capture anime name from submit button attribute & print out anime name
 function animeName() {
     let aName = $(this).attr("data-name");
-    console.log(aName);
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + aName + "&api_key=EFMrrRT2uCQGsGGwxiSQ6pKBtrBNExmJ&limit=10";
+
+    //AJAX CALL
+
+
+    $.ajax({
+        url: queryURL,
+        methodL: "GET"
+    }).then(function(giphy) {
+
+        let rating = giphy.rating;
+        let gif = giphy.images.fixed_width.url;
+        let img = giphy.images.fixed_width_still.url;
+
+        $('#anime').append(img);
+
+    });
 }
 
 //this function renders the buttons
@@ -24,23 +40,32 @@ function renderButtons() {
         newbutton.text(anime[i]);
 
         $("#animeButtons").append(newbutton);
-
+        newbutton.css({"margin": "5px"});
     }
 
 }
 
 //events
 
-//this function handles the event submit click
+//this  handles the event submit click
 
 $("#submit").on("click", function(event){
 
     event.preventDefault();
 
-    let animeInput = $("anime-input").val().trim();
+    let animeInput = $("#anime-input").val().trim();
 
+    //add new anime to the array
     anime.push(animeInput);
+
+    //call render buttons to process the array
+    renderButtons();
 })
+
+$(document).on("click", ".anime", animeName);
+
+//display initial buttons
+renderButtons();
 
 
 });
