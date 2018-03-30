a3d1766005cb95ee7c7cd021a116f829c87afbf5
Bridgette Smith <bsmith@ift.org>
2018-03-30 00:25:44 -0500

    Temporary removed AJAX Call to fix renderButtons function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,42 +4,28 @@ $(document).ready(function() {
 let anime = ["Cowboy Bebop", "Dragonball Z", "Sailormoon", "Naruto", "One Piece", "Fullmetal Alchemist: Brotherhood", "Bleach", "Pokemon", "Kill la Kill"];
 
 //capture anime name from submit button attribute & print out anime name
-function animeName() {
-    let aName = $(this).attr("data-name");
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + aName + "&api_key=EFMrrRT2uCQGsGGwxiSQ6pKBtrBNExmJ&limit=10";
 
-    //AJAX CALL
-
-
-    $.ajax({
-        url: queryURL,
-        methodL: "GET"
-    }).then(function(giphy) {
-
-        let rating = giphy.rating;
-        let gif = giphy.images.fixed_width.url;
-        let img = giphy.images.fixed_width_still.url;
-
-        $('#anime').append(img);
-
-    });
-}
-
-//this function renders the buttons
 
 function renderButtons() {
 
+    //delete current buttons and replace with new buttons
     $("#animeButtons").empty();
 
     for (var i = 0; i < anime.length; i++) {
 
+       // generate a new button with type and class
         let newbutton = $('<button type="button" class="btn btn-info">');
 
-        newbutton.attr("dataname", anime[i]);
+        //assign data-attribute the name of the string in the array
+        newbutton.attr("data-name", anime[i]);
 
+        //assign the name of the button = string in the array
         newbutton.text(anime[i]);
-
+    
+        //append the button to the div
         $("#animeButtons").append(newbutton);
+
+        //create spacing of button in html
         newbutton.css({"margin": "5px"});
     }
 
@@ -47,22 +33,23 @@ function renderButtons() {
 
 //events
 
-//this  handles the event submit click
-
-$("#submit").on("click", function(event){
-
+//this handles the event submit click
+$("#addAnime").on("click", function(event){
+    //prvent form from trying to submit itself
     event.preventDefault();
 
+    //create a variable for the form input
     let animeInput = $("#anime-input").val().trim();
 
-    //add new anime to the array
+
+    //add form input into array
     anime.push(animeInput);
 
-    //call render buttons to process the array
+    //call render buttons to create a new button based off of input
     renderButtons();
 })
 
-$(document).on("click", ".anime", animeName);
+//$(document).on("click", ".anime-btn", displayAnime);
 
 //display initial buttons
 renderButtons();
