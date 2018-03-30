f1792994ff6766c76b05537ebb4c795dc85e0b80
Bridgette Smith <bsmith@ift.org>
2018-03-30 02:09:29 -0500

    Clean up index file and added last remaining script to get app working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,20 +21,45 @@ function displayAnime() {
 
         //empty out anime in area
         $("#anime").empty();
+        $('#anime').append('<h5>Click on the gif to see it animate!</h5><br/>');
+        
 
         //cycle through 10 items
         for (let j =  0; j < 10; j++ ) {
 
         let rating = $("<p>").text('Gif Rating: ' + giphy.data[j].rating);
        // let gif = $("<img>"giphy.images.fixed_width.url;
-        let img = $("<img>").attr({"src": giphy.data[j].images.fixed_height_still.url, "data-still":  giphy.data[j].images.fixed_height_still.url, "data-animate": giphy.data[j].images.fixed_height.url});
+        let img = $("<img class='anime-gif'>").attr({"src": giphy.data[j].images.fixed_height_still.url, "data-still":  giphy.data[j].images.fixed_height_still.url, "data-animate": giphy.data[j].images.fixed_height.url});
+        let newDiv = $("<div>").css({"float": "left", "margin": "5px"});
 
+        $('#anime').append(newDiv);
        
-        $('#anime').append(rating, img);
+        newDiv.append(rating, img);
+
+     
+       
+
         renderButtons();
 
+        //anime image play function
+            $(".anime-gif").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            }
+        });
+       
        
         }
+       
 
     });
 }
